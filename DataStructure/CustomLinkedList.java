460e3de8712e365bb2c04ec4b1ed86c2672ed2e7
ADlv19 <adwaitg02@gmail.com>
2022-08-04 23:59:44 +0530

    idk

--- DataStructure/CustomLinkedList.java
+++ DataStructure/CustomLinkedList.java
@@ -2,8 +2,35 @@ package DataStructure;
 
 public class CustomLinkedList {
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public class Node{
+
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
 
 }
